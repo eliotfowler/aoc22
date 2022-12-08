7c25243bccf973b8218452055e0db5b9043a38cb
Eliot Fowler <eliot@eliotfowler.com>
2022-12-08 08:28:42 -0800

    Day 8 (cleanup)

--- Sources/aoc22/day8.swift
+++ Sources/aoc22/day8.swift
@@ -10,78 +10,66 @@ let day8 = Day { part, input in
 private func part1(_ input: String) -> Int {
   let rows = input
     .components(separatedBy: "\n")
-    .map { Array($0).map { (Int(String($0))!, Bool?.none) } }
+    .map { Array($0).compactMap { Int(String($0)) } }
   
-  var lookingLeft = Array(repeating: Array(repeating: false, count: rows.count), count: rows.count)
-  var lookingRight = Array(repeating: Array(repeating: false, count: rows.count), count: rows.count)
-  var lookingUp = Array(repeating: Array(repeating: false, count: rows.count), count: rows.count)
-  var lookingDown = Array(repeating: Array(repeating: false, count: rows.count), count: rows.count)
+  let leftIndexes = (0 ..< rows.count).flatMap { row in (0 ..< rows.count).map { column in (row, column) } }
+  let lookingLeft = scanForVisibleTrees(rows: rows, indexes: leftIndexes)
+  print(lookingLeft)
   
-  for rowIndex in 0 ..< rows.count {
-    let row = rows[rowIndex]
-    var rowMax = -1
-    for columnIndex in 0 ..< row.count {
-      let tree = rows[rowIndex][columnIndex]
-      lookingLeft[rowIndex][columnIndex] = tree.0 > rowMax ? true : false
-      rowMax = max(rowMax, tree.0)
+  let rightIndexes = (0 ..< rows.count).flatMap { row in (0 ..< rows.count).reversed().map { column in (row, column) } }
+  let lookingRight = scanForVisibleTrees(rows: rows, indexes: rightIndexes)
+  
+  let upIndexes = (0 ..< rows.count).flatMap { column in (0 ..< rows.count).map { row in (row, column) } }
+  let lookingUp = scanForVisibleTrees(rows: rows, indexes: upIndexes)
+  
+  let downIndexes = (0 ..< rows.count).flatMap { column in (0 ..< rows.count).reversed().map { row in (row, column) } }
+  let lookingDown = scanForVisibleTrees(rows: rows, indexes: downIndexes)
+  
+  let numVisible = rows.enumerated()
+    .flatMap { rowIndex, row in
+      row.enumerated().map { columnIndex, _ in
+        lookingLeft[rowIndex][columnIndex] || lookingRight[rowIndex][columnIndex] ||
+        lookingUp[rowIndex][columnIndex] || lookingDown[rowIndex][columnIndex]
+      }
     }
-  }
-  
-  for r in 0 ..< rows.count {
-    let rowIndex = rows.count - 1 - r
-    let row = rows[rowIndex]
-    var rowMax = -1
-    for c in 0 ..< row.count {
-      let columnIndex = row.count - 1 - c
-      let tree = rows[rowIndex][columnIndex]
-      lookingRight[rowIndex][columnIndex] = tree.0 > rowMax ? true : false
-      rowMax = max(rowMax, tree.0)
-    }
-  }
-  
-  for columnIndex in 0 ..< rows[0].count {
-    var columnMax = -1
-    for rowIndex in 0 ..< rows.count {
-      let tree = rows[rowIndex][columnIndex]
-      lookingUp[rowIndex][columnIndex] = tree.0 > columnMax ? true : false
-      columnMax = max(columnMax, tree.0)
-    }
-  }
-  
-  for c in 0 ..< rows[0].count {
-    let columnIndex = rows[0].count - 1 - c
-    var columnMax = -1
-    for r in 0 ..< rows.count {
-      let rowIndex = rows.count - 1 - r
-      let tree = rows[rowIndex][columnIndex]
-      lookingDown[rowIndex][columnIndex] = tree.0 > columnMax ? true : false
-      columnMax = max(columnMax, tree.0)
-    }
-  }
-  
-  let numVisible = rows.enumerated().flatMap { rowIndex, row in
-    row.enumerated().map { columnIndex, _ in
-      lookingLeft[rowIndex][columnIndex] || lookingRight[rowIndex][columnIndex] ||
-      lookingUp[rowIndex][columnIndex] || lookingDown[rowIndex][columnIndex]
-    }
-  }
     .reduce(0) { $0 + ($1 ? 1 : 0)}
   
   return numVisible
 }
 
-typealias ScenicView = (numVisible: Int, blockedByIndex: Int)
+private func scanForVisibleTrees(rows: [[Int]], indexes: [(Int, Int)]) -> [[Bool]] {
+  var visibleTrees = Array(repeating: Array(repeating: false, count: rows.count), count: rows.count)
+  var runningMax = -1
+  for (rowIndex, columnIndex) in indexes {
+    let tree = rows[rowIndex][columnIndex]
+    if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
+      visibleTrees[rowIndex][columnIndex] = true
+      runningMax = tree
+    } else {
+      visibleTrees[rowIndex][columnIndex] = tree > runningMax ? true : false
+      runningMax = max(runningMax, tree)
+    }
+  }
+  
+  return visibleTrees
+}
 
 private func part2(_ input: String) -> Int {
   let rows = input
     .components(separatedBy: "\n")
     .map { Array($0).compactMap { Int(String($0)) } }
   
+  let leftIndexes = (0 ..< rows.count - 1).flatMap { row in (0 ..< rows.count).map { column in (row, column) } }
+  let lookingLeft = scanForScenicViews(rows: rows, indexes: leftIndexes)
   
-  let lookingLeft = part2LookingLeft(rows: rows)
-  let lookingRight = part2LookingRight(rows: rows)
-  let lookingUp = part2LookingUp(rows: rows)
-  let lookingDown = part2LookingDown(rows: rows)
+  let rightIndexes = (0 ..< rows.count - 1).flatMap { row in (0 ..< rows.count).reversed().map { column in (row, column) } }
+  let lookingRight = scanForScenicViews(rows: rows, indexes: rightIndexes)
+  
+  let upIndexes = (0 ..< rows.count - 1).flatMap { column in (0 ..< rows.count).map { row in (row, column) } }
+  let lookingUp = scanForScenicViews(rows: rows, indexes: upIndexes)
+  
+  let downIndexes = (0 ..< rows.count - 1).flatMap { column in (0 ..< rows.count).reversed().map { row in (row, column) } }
+  let lookingDown = scanForScenicViews(rows: rows, indexes: downIndexes)
   
   var maxScenicScore = 0
   for rowIndex in 0 ..< rows.count {
@@ -95,102 +83,26 @@ private func part2(_ input: String) -> Int {
   return maxScenicScore
 }
 
-private func part2LookingLeft(rows: [[Int]]) -> [[ScenicView]] {
-  var lookingLeft: [[ScenicView]] = Array(repeating: Array(repeating: (0, 0), count: rows.count), count: rows.count)
-  for rowIndex in 0 ..< rows.count {
-    let row = rows[rowIndex]
-    for columnIndex in 0 ..< row.count {
-      if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
-        lookingLeft[rowIndex][columnIndex] = (numVisible: 0, blockedByIndex: 0)
-      } else {
-        let treeHeight = rows[rowIndex][columnIndex]
-        var prevColumnIndex = columnIndex - 1
-        var numVisible = 1
-        while treeHeight > rows[rowIndex][prevColumnIndex], prevColumnIndex > 0 {
-          let prevItem = lookingLeft[rowIndex][prevColumnIndex]
-          numVisible += prevItem.numVisible
-          prevColumnIndex = prevItem.blockedByIndex
-        }
-        lookingLeft[rowIndex][columnIndex] = (numVisible: numVisible, blockedByIndex: prevColumnIndex)
+typealias ScenicView = (numVisible: Int, blockedByIndex: Int)
+private func scanForScenicViews(rows: [[Int]], indexes: [(Int, Int)]) -> [[ScenicView]] {
+  var scenicViews: [[ScenicView]] = Array(repeating: Array(repeating: (0, 0), count: rows.count), count: rows.count)
+  for (index, (rowIndex, columnIndex)) in indexes.enumerated() {
+    if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
+      scenicViews[rowIndex][columnIndex] = (numVisible: 0, blockedByIndex: 0)
+    } else {
+      let treeHeight = rows[rowIndex][columnIndex]
+      var prevIndex = index - 1
+      var numVisible = 1
+      while treeHeight > rows[indexes[prevIndex].0][indexes[prevIndex].1], prevIndex > 0 {
+        let prevItem = scenicViews[indexes[prevIndex].0][indexes[prevIndex].1]
+        numVisible += prevItem.numVisible
+        prevIndex = prevItem.blockedByIndex
       }
+      scenicViews[rowIndex][columnIndex] = (numVisible: numVisible, blockedByIndex: prevIndex)
     }
   }
   
-  return lookingLeft
-}
-
-private func part2LookingRight(rows: [[Int]]) -> [[ScenicView]] {
-  var lookingRight: [[ScenicView]] = Array(repeating: Array(repeating: (0, 0), count: rows.count), count: rows.count)
-  for r in 0 ..< rows.count {
-    let rowIndex = rows.count - 1 - r
-    let row = rows[rowIndex]
-    for c in 0 ..< row.count {
-      let columnIndex = row.count - 1 - c
-      if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
-        lookingRight[rowIndex][columnIndex] = (numVisible: 0, blockedByIndex: 0)
-      } else {
-        let treeHeight = rows[rowIndex][columnIndex]
-        var prevColumnIndex = columnIndex + 1
-        var numVisible = 1
-        while treeHeight > rows[rowIndex][prevColumnIndex], prevColumnIndex < row.count - 1 {
-          let prevItem = lookingRight[rowIndex][prevColumnIndex]
-          numVisible += prevItem.numVisible
-          prevColumnIndex = prevItem.blockedByIndex
-        }
-        lookingRight[rowIndex][columnIndex] = (numVisible: numVisible, blockedByIndex: prevColumnIndex)
-      }
-    }
-  }
-  
-  return lookingRight
-}
-
-private func part2LookingUp(rows: [[Int]]) -> [[ScenicView]] {
-  var lookingUp: [[ScenicView]] = Array(repeating: Array(repeating: (0, 0), count: rows.count), count: rows.count)
-  for columnIndex in 0 ..< rows[0].count {
-    for rowIndex in 0 ..< rows.count {
-      if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
-        lookingUp[rowIndex][columnIndex] = (numVisible: 0, blockedByIndex: 0)
-      } else {
-        let treeHeight = rows[rowIndex][columnIndex]
-        var prevRowIndex = rowIndex - 1
-        var numVisible = 1
-        while treeHeight > rows[prevRowIndex][columnIndex], prevRowIndex > 0 {
-          let prevItem = lookingUp[prevRowIndex][columnIndex]
-          numVisible += prevItem.numVisible
-          prevRowIndex = prevItem.blockedByIndex
-        }
-        lookingUp[rowIndex][columnIndex] = (numVisible: numVisible, blockedByIndex: prevRowIndex)
-      }
-    }
-  }
-  
-  return lookingUp
-}
-
-private func part2LookingDown(rows: [[Int]]) -> [[ScenicView]] {
-  var lookingDown: [[ScenicView]] = Array(repeating: Array(repeating: (0, 0), count: rows.count), count: rows.count)
-  for c in 0 ..< rows[0].count {
-    let columnIndex = rows[0].count - 1 - c
-    for r in 0 ..< rows.count {
-      let rowIndex = rows.count - 1 - r
-      if rows.isElementOnEdge(row: rowIndex, column: columnIndex) {
-        lookingDown[rowIndex][columnIndex] = (numVisible: 0, blockedByIndex: 0)
-      } else {
-        let treeHeight = rows[rowIndex][columnIndex]
-        var prevRowIndex = rowIndex + 1
-        var numVisible = 1
-        while treeHeight > rows[prevRowIndex][columnIndex], prevRowIndex < rows[0].count - 1 {
-          let prevItem = lookingDown[prevRowIndex][columnIndex]
-          numVisible += prevItem.numVisible
-          prevRowIndex = prevItem.blockedByIndex
-        }
-        lookingDown[rowIndex][columnIndex] = (numVisible: numVisible, blockedByIndex: prevRowIndex)
-      }
-    }
-  }
-  
-  return lookingDown
+  return scenicViews
 }
 
 extension Array where Element == Array<Int> {
